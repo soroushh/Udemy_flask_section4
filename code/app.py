53cb32f3f8466f75d632a2883484a2578a2f1b44
student <sorush.kh68@gmail.com>
2019-07-02 11:38:02 +0100

    We can have the items list now.

--- code/app.py
+++ code/app.py
@@ -13,17 +13,21 @@ class Item(Resource):
         # return(list(filter(lambda item: item["name"]== name , items)))
         chosen_items = [item for item in items if item["name"] == name]
         if chosen_items == []:
-            return({"message":"item not found"})
+            return({"message":"item not found"}), 404
         else:
-            return(chosen_items[0])
+            return chosen_items[0], 200
 
     def post(self, name):
         items.append({"name":name , "price":12})
-        return(jsonify({"message": "Item is created"}))
+        return {"name":name , "price" :12}, 201
 
-
+class Items(Resource):
+    def get(self):
+        return {"items":items}
 
 
 api.add_resource(Item , '/item/<string:name>')
+
+api.add_resource(Items , '/items')
 
 app.run( port = 5000 )
