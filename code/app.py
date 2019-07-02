ce04ba7709ffd5e803c7982bd7980979ea4e03d9
student <sorush.kh68@gmail.com>
2019-07-02 11:20:27 +0100

    We are able to create and get an item.

--- code/app.py
+++ code/app.py
@@ -3,16 +3,27 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    students = []
+items = []
+
+class Item(Resource):
     def get(self,name):
-        return {'student':name}
-    def post(self):
-        data = request.get_json()
-        self.students.append({"name":data["name"]})
-        return(jsonify({"student":self.students}))
+        # for item in items:
+        #     if item["name"]== name :
+        #         return(jsonify(item))
+        # return(list(filter(lambda item: item["name"]== name , items)))
+        chosen_items = [item for item in items if item["name"] == name]
+        if chosen_items == []:
+            return({"message":"item not found"})
+        else:
+            return(chosen_items[0])
+
+    def post(self, name):
+        items.append({"name":name , "price":12})
+        return(jsonify({"message": "Item is created"}))
 
 
-api.add_resource(Student , '/student/<string:name>', '/student')
+
+
+api.add_resource(Item , '/item/<string:name>')
 
 app.run( port = 5000 )
