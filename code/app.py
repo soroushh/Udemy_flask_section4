91efd9660f9b9a167aacfcd3d512478dd017ba29
student <sorush.kh68@gmail.com>
2019-07-03 11:18:59 +0100

    Some refactoring to the code was done.

--- code/app.py
+++ code/app.py
@@ -9,28 +9,23 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+    "price",
+    type=float ,
+    required = True,
+    help="The price should be float and it is required to have it in your request."
+    )
+
     @jwt_required()
     def get(self,name):
         item = next(filter(lambda item: item["name"]== name , items), None)
         return {"item":item} ,200 if item else 404
 
     def post(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-        "price",
-        type=float ,
-        required = True,
-        help="The price should be float and it is required to have it in your request."
-        )
-        parser.add_argument(
-        "end",
-        type=float ,
-        required = True,
-        help="The price should be float and it is required to have it in your request."
-        )
-        request_data = parser.parse_args()
         if next(filter(lambda item: item["name"]== name , items), None):
             return {"message":"Item exists already"}, 400
+        request_data = Item.parser.parse_args()
         items.append({"name":name , "price":request_data["price"]})
         return {"name":name , "price" :request_data["price"]}, 201
     def delete(self,name):
@@ -38,12 +33,7 @@ class Item(Resource):
         items = list(filter(lambda item: item["name"] != name , items))
         return({"message": "The '{}' item was deleted." .format(name)})
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-        type = float,
-        required=True,
-        help="This field can not be left blank. In addition it should be float type.")
-        request_data = parser.parse_args()
+        request_data = Item.parser.parse_args()
         item = next(filter(lambda item: item["name"] == name , items), None)
         if item is None:
             item = {"name": name , "price": request_data["price"]}
